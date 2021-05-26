2ffcaad75988e389f0e1188172e789d917428708
madosuki <yurikasamaaikatsu@gmail.com>
2021-05-26 12:48:44 +0900

    update

--- main.asm
+++ main.asm
@@ -8,17 +8,28 @@ fmt:   db  "%d", 0x0A, 0
     section .text   
     global _start
 
-   
-
+gets:
+    push rax
+    xor rax, rax                ; syscall no 0
+    xor rdi, rdi                ; fd no 0 = stdin
+    lea rsi, [rel arr]          ; set buffer
+    mov rdx, 0x0A               ; set buffer size
+    syscall
+    pop rax
+    xor rdx, rdx
+    xor rsi, rsi
+    ret
+    
+    
 atoi:
+    push rax
+    xor rax, rax
     xor rcx, rcx                ; int i = 0
     .detect_digit_loop: nop
     inc rcx
     cmp [arr+rcx-1], byte 0xA   ; detect lf code
     jg .detect_digit_loop       ; if (char > 0)
     dec rcx
-    mov rax, rcx
-    xor rax, rax
     xor rdi, rdi                ; int times
     inc rdi                     ; same times = 1
     .convert: nop
@@ -42,55 +53,43 @@ atoi:
     cmp rcx, 0x0                ; if rcx == 0
     jne .convert
     xor rdi, rdi
-    xor rbx, rbx
+    mov rsi, rax
+    pop rax
     ret
 
-;; print_int:
-;;     push rax
-;;     xor rax, rax                ; syscall no 0
-;;     xor rdi, rdi                ; fd no 0 = stdin
-;;     lea rsi, [rel arr]          ; set buffer
-;;     mov rdx, 0x0A               ; set buffer size
-;;     syscall
-;;     call atoi
-;;     mov rsi, rax
-;;     mov rdi, fmt
-;;     call printf
-;;     pop rax
-;;     ret
-
-
 fact:
-    mov rcx, rax               ; set target number to rcx, specified 5 here. rcx is counter register.
-    xor rax, rax
+    mov rcx, rsi               ; set target number to rcx. rcx is counter register.
+    xor rax, rax               ; init rax
     inc rax
     .loop mul rcx               ; .loop label and multiply rax * rcx.
-    xor rax, rdx
+    xor rax, rdx                ; combine mul result
     dec rcx                     ; decrement rcx.
     cmp rcx, 0x01               ; compare between rcx and 0x01
     jg .loop                    ; goto .loop label when result of compare is greater than 0x01.
     xor rcx, rcx                ; init rcx, same rcx = 0
     ret                         ; exit function
 
-fact_with_stdin:
+print:
     push rax
-    xor rax, rax                ; syscall no 0
-    xor rdi, rdi                ; fd no 0 = stdin
-    lea rsi, [rel arr]          ; set buffer
-    mov rdx, 0x0A               ; set buffer size
-    syscall
-    call atoi
-    call fact
-    mov rsi, rax                ; copy from rax to rsi.
-    mov rdi, fmt                ; set printf format.
     call printf                 ; call printf function
     pop rax
     ret
     
     
-_start:
+fact_with_stdin:
+    push rax
     xor rax, rax
-    mov rdx, rax
+    call gets
+    call atoi
+    call fact
+    mov rsi, rax
+    mov rdi, fmt
+    call print
+    pop rax
+    ret
+    
+    
+_start:
     call fact_with_stdin
     xor rdi, rdi                ; init 0
     mov rax, rdi                ; set return value
